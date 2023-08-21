053f918ab3f30eed46fc5d358274eb3ff3ab8b5e
songzhanpeng <songzhanpeng@iauto.com>
2023-08-21 15:04:51 +0800

    feat:  新增 命令面板 设置下班时间

--- README.md
+++ README.md
@@ -1,51 +1,41 @@
 # 下班提醒插件（go-off-work）
 
-这是你的扩展 "下班提醒插件（go-off-work）" 的 README 文件。在写完简要描述后，我们建议包含以下几个部分。
+这是你的扩展 "下班提醒插件（go-off-work）" 的 README 文件。
 
 ## 功能
 
-描述你的扩展的具体功能，包括扩展在操作中的截图。图片路径是相对于这个 README 文件的。
-
-例如，如果在你的扩展项目工作区下有一个名为 "images" 的图像子文件夹：
-
-![功能 X](images/feature-x.png)
-
-> 提示：许多流行的扩展使用动画来展示扩展！我们建议使用简短、集中的动画，易于跟随。
+该插件可以在状态栏中显示距离下班还有多少时间，并在下班时间到达时提醒你。
 
 ## 需求
 
-如果你的扩展有任何要求或依赖项，请在这个部分描述如何安装和配置它们。
+无特殊需求或依赖项。
 
 ## 扩展设置
 
-如果你的扩展通过 `contributes.configuration` 扩展点添加了任何 VS Code 设置，请在这个部分进行说明。
+该扩展支持以下设置：
 
-例如：
+- `goOffWork.hour`：设置下班的小时数。
+- `goOffWork.minute`：设置下班的分钟数。
 
-这个扩展添加了以下设置：
+你可以通过修改这些设置来自定义下班时间。
 
-- `myExtension.enable`：启用/禁用这个扩展。
-- `myExtension.thing`：设置为 `blah` 以执行某些操作。
+## 使用方法
 
-## 已知问题
-
-指出已知的问题可以帮助用户避免重复报告。
+在 Visual Studio Code 中，你可以按下 `Ctrl+Shift+P`（或 `Cmd+Shift+P`）来打开命令面板，然后输入 "设置下班时间" 并选择它，即可修改下班时间。
 
 ## 发布说明
 
-用户在更新扩展时会关注发布说明。
+### 0.0.1
 
-### 1.0.0
+初始发布，提供基本的下班提醒功能。
 
-初始发布...
+### 0.0.2
 
-### 1.0.1
+修复扩展加载后不显示时间问题
 
-修复问题 #。
+### 0.0.3
 
-### 1.1.0
-
-添加了 X、Y 和 Z 功能。
+支持命令  `设置下班时间` 时间函数补零
 
 ---
 
@@ -54,18 +44,5 @@
 确保你已阅读扩展准则，并遵循最佳实践来创建你的扩展。
 
 * [扩展准则](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## 使用 Markdown
-
-你可以使用 Visual Studio Code 来编辑你的 README。以下是一些有用的编辑器快捷键：
-
-* 分割编辑器（在 macOS 上是 `Cmd+\`，在 Windows 和 Linux 上是 `Ctrl+\`）。
-* 切换预览（在 macOS 上是 `Shift+Cmd+V`，在 Windows 和 Linux 上是 `Shift+Ctrl+V`）。
-* 按下 `Ctrl+Space`（Windows、Linux、macOS）可以看到 Markdown 片段的列表。
-
-## 获取更多信息
-
-* [Visual Studio Code 的 Markdown 支持](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown 语法参考](https://help.github.com/articles/markdown-basics/)
 
 **尽情享受！**
--- src/extension.ts
+++ src/extension.ts
@@ -1,55 +1,107 @@
 import * as vscode from 'vscode';
 
-const allConfig = vscode.workspace.getConfiguration();
-const config = allConfig.goOffWork;
-
 const GetOffMessage = '已经下班啦~ 赶紧滚回家去';
 const NotificationMessage = '到点啦~ 该下班了!';
 
-/** 获取提示消息 */
-function getMessage() {
-    const now = new Date();
-    const goOffWork = new Date();
-    goOffWork.setHours(config.hour);
-    goOffWork.setMinutes(config.minute);
-    goOffWork.setSeconds(0);
-    
-	const duration = goOffWork.getTime() - now.getTime();
-    if (duration <= 0) {
-        return GetOffMessage;
-    }
+let timer: NodeJS.Timeout | undefined;
+let statusBar: vscode.StatusBarItem | undefined;
 
-    const hour = Math.floor(duration / 1000 / 60 / 60);
-    const minute = Math.floor(duration / 1000 / 60 % 60);
-    const second = Math.floor(duration / 1000 % 60);
+// 创建并显示状态栏消息
+function showStatusBarMessage(message: string) {
+	if (statusBar) {
+		statusBar.dispose();
+	}
 
-    return `>> 距离下班还有 ${hour ? hour + '小时' : ''}${minute ? minute + '分钟' : ''}${second ? second + '秒' : ''}`;
+	statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+	statusBar.text = message;
+	statusBar.show();
 }
 
-/** 是否已经提醒过下班 */
-let isGetOff = false;
+// 获取提示消息
+function getMessage(config: any) {
+	const now = new Date();
+	const goOffWork = new Date();
+	goOffWork.setHours(config.hour);
+	goOffWork.setMinutes(config.minute);
+	goOffWork.setSeconds(0);
+	
+	const duration = goOffWork.getTime() - now.getTime();
+	if (duration <= 0) {
+		return GetOffMessage;
+	}
+
+	const hour = Math.floor(duration / 1000 / 60 / 60);
+	const minute = Math.floor(duration / 1000 / 60 % 60);
+	const second = Math.floor(duration / 1000 % 60);
+
+	// 补零函数，确保时间显示两位数
+	const addLeadingZero = (num: number) => num.toString().padStart(2, '0');
+
+	return `>> 距离下班还有 ${hour ? addLeadingZero(hour) + '小时' : ''}${minute ? addLeadingZero(minute) + '分钟' : ''}${second ? addLeadingZero(second) + '秒' : ''}`;
+}
+
+// 更新状态栏项
+function updateStatusBarItem(config: any) {
+	const newMessage = getMessage(config);
+	showStatusBarMessage(newMessage);
+
+	if (newMessage === GetOffMessage) {
+		vscode.window.showInformationMessage(NotificationMessage);
+		clearInterval(timer!);
+	} else {
+		timer = setTimeout(() => {
+			statusBar?.dispose();
+			updateStatusBarItem(config);
+		}, 1000);
+	}
+}
+
+// 设置下班时间的命令
+function setGoOffWorkTime() {
+	const config = vscode.workspace.getConfiguration().get('goOffWork');
+	vscode.window.showInputBox({ placeHolder: '请输入下班时间（HH:mm）' }).then(inputValue => {
+		if (inputValue) {
+			const [hour, minute] = inputValue.split(':');
+			if (!isNaN(Number(hour)) && !isNaN(Number(minute))) {
+				vscode.workspace.getConfiguration().update('goOffWork.hour', Number(hour), vscode.ConfigurationTarget.Global);
+				vscode.workspace.getConfiguration().update('goOffWork.minute', Number(minute), vscode.ConfigurationTarget.Global);
+				clearInterval(timer!);
+				updateStatusBarItem(config);
+			} else {
+				vscode.window.showErrorMessage('请输入有效的时间格式（HH:mm）！');
+			}
+		}
+	});
+}
 
 export function activate(context: vscode.ExtensionContext) {
-	const myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+	const allConfig = vscode.workspace.getConfiguration();
+	let config = allConfig.goOffWork;
 
-	myStatusBarItem.text = getMessage(),
-	myStatusBarItem.show();
+	if (!config || typeof config.hour !== 'number' || typeof config.minute !== 'number') {
+		vscode.window.showErrorMessage('请正确配置 goOffWork 的 hour 和 minute！');
+		return;
+	}
 
-	setInterval(() => {
-		const newMessage = getMessage();
-		myStatusBarItem.text = newMessage;
-		if (newMessage === GetOffMessage) {
-			if (!isGetOff) {
-				vscode.window.showInformationMessage(NotificationMessage);
-				isGetOff = true;
-			}
-		} else {
-			isGetOff = false;
+	updateStatusBarItem(config);
+
+	// 监听配置变化，在配置变化时重新加载插件
+	vscode.workspace.onDidChangeConfiguration((event) => {
+		if (event.affectsConfiguration('goOffWork')) {
+			config = vscode.workspace.getConfiguration().get('goOffWork');
+			updateStatusBarItem(config);
 		}
-	}, 1000);
+	});
 
-	context.subscriptions.push(myStatusBarItem);
+	// 注册设置下班时间的命令
+	context.subscriptions.push(vscode.commands.registerCommand('setGoOffWorkTime', setGoOffWorkTime));
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	if (timer) {
+		clearTimeout(timer);
+	}
+	if (statusBar) {
+		statusBar.dispose();
+	}
+}
